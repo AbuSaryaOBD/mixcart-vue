146ac3baf92817370d45c97a3b029f8ea15ac8d5
Abu Sarya <abukarmo194@gmail.com>
2021-07-04 15:25:24 +0300

    Lazy load image

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="max-w-sm rounded overflow-hidden shadow-lg m-2">
     <img
-      class="w-full"
+      class="w-full h-60 bg-gray-400"
+      :class="animatePulse"
       :src="`https://mixcart.com.tr/storage/${product.images[0].image}`"
+      @load="imageLoaded"
       alt="Sunset in the mountains"
     />
     <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">{{ product.name }}</div>
+      <div class="font-bold text-xl mb-2 overflow-hidden h-8">{{ product.name }}</div>
       <div class="text-lg mb-1">{{ product.price.toFixed(2) }}</div>
       <StarRating 
         :rating="product.average_rate"
@@ -32,5 +34,15 @@ export default {
       required: true,
     },
   },
+  data(){
+    return {
+      animatePulse: ' animate-pulse'
+    }
+  },
+  methods:{
+    imageLoaded(){
+      this.animatePulse = ''
+    }
+  }
 };
 </script>
