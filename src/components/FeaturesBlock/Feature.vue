5c1ca8db38133e5694b6a774323bac449f71a1fd
Abu Sarya <abukarmo194@gmail.com>
2021-07-11 15:36:34 +0300

    Feature Right Border

--- src/components/FeaturesBlock/Feature.vue
+++ src/components/FeaturesBlock/Feature.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col items-center overflow-hidden">
+    <div class="flex flex-col items-center overflow-hidden" :id="`feature-${index}`">
         <h3 class="text-center font-bold mb-auto px-3">{{ title }}</h3>
-        <div class="w-full">
+        <div class="w-full" :class="{ 'border-r-2': hasBorder }">
             <img :src="path" class="w-24 h-24 object-cover mt-5 mx-auto">
         </div>
         <p class="text-gray-600 text-center mt-3 px-3">{{ desc }}</p>
@@ -14,6 +14,20 @@ export default {
         title: String,
         path: String,
         desc: String,
+        index: Number,
     },
+    data() {
+        return {
+            hasBorder: false,
+        }
+    },
+    mounted(){
+        if (this.index > 0) {
+            const myTop = this.$el.getBoundingClientRect().top
+            const sibling = document.getElementById(`feature-${this.index - 1}`)
+            const prevTop = sibling.getBoundingClientRect().top
+            this.hasBorder = myTop === prevTop
+        }
+    }
 }
 </script>
--- src/components/FeaturesBlock/FeaturesBlock.vue
+++ src/components/FeaturesBlock/FeaturesBlock.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="flex flex-wrap justify-center">
-        <feature v-for="feature in features" :key="feature.id"
+    <div class="flex flex-wrap justify-center w-10/12 mx-auto">
+        <feature v-for="feature, index in features" :key="feature.id"
             class="xs:w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mt-5 seperator"
+            :index="index"
             :title="feature.title"
             :path="imagePath(feature.image)"
             :desc="feature.desc"
